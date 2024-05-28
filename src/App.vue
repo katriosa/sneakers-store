111f5b2e13a845293c3022de2fa027c3d3cead82
katriosa <ee.chistyakova@gmail.com>
2024-05-28 22:28:16 +0200

    feat: implement Card

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import MainHeader from './components/MainHeader.vue'
+import CardItem from './components/CardItem.vue'
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto mt-14 h-screen rounded-xl shadow-xl">
+  <div class="bg-white w-4/5 m-auto mt-14 rounded-xl shadow-xl">
     <MainHeader />
+    <CardItem />
   </div>
 </template>
