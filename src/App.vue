b611c6d7c6ff69350d81ab57a4dd2d7ad547a80e
katriosa <ee.chistyakova@gmail.com>
2024-05-28 19:44:41 +0200

    feat: implement header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<script setup></script>
+<script setup>
+import MainHeader from './components/MainHeader.vue'
+</script>
 
 <template>
   <div class="bg-white w-4/5 m-auto mt-14 h-screen rounded-xl shadow-xl">
-    <h1>Title</h1>
+    <MainHeader />
   </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Sneakers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/cart.svg" alt="Cart" />
+        <strong>1205 руб.</strong>
+      </li>
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/heart.svg" alt="Heart" />
+        <span>Закладки</span>
+      </li>
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/profile.svg" alt="Profile" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
