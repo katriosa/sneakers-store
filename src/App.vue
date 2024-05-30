04cc95858d9969d9162eb5b1ac53390ccb138b33
katriosa <ee.chistyakova@gmail.com>
2024-05-30 15:03:25 +0200

    feat: implement search and sort controls

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
 import MainHeader from './components/MainHeader.vue'
 import CardList from './components/CardList.vue'
-import Drawer from './components/drawer/Drawer.vue'
+import SearchSortControls from './components/SearchSortControls.vue'
+// import Drawer from './components/drawer/Drawer.vue'
 </script>
 
 <template>
-  <Drawer />
+  <!-- <Drawer /> -->
   <div class="bg-white w-4/5 m-auto mt-14 rounded-xl shadow-xl">
     <MainHeader />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+        <SearchSortControls />
+      </div>
       <CardList />
     </div>
   </div>
---
+++ src/components/SearchSortControls.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex gap-4">
+    <select class="py-2 px-3 border rounded-md outline-none">
+      <option>По названию</option>
+      <option>По цене (дешевые)</option>
+      <option>По цене (дорогие)</option>
+    </select>
+
+    <div class="relative">
+      <img class="absolute left-4 top-3" src="/search.svg" />
+      <input
+        class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400"
+        type="text"
+        placeholder="Поиск..."
+      />
+    </div>
+  </div>
+</template>
